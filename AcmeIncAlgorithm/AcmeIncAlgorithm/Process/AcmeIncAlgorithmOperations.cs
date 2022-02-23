040de38ad6d36271d730613dfb7fbf2ef436c52f
Ari Bautista <aribautista666@gmail.com>
2022-02-22 21:39:28 -0800

    Se agrego repositorio completo

---
+++ AcmeIncAlgorithm/AcmeIncAlgorithm/Process/AcmeIncAlgorithmOperations.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AcmeIncAlgorithm
+{
+    public class AcmeIncAlgorithmOperations
+    {
+        public int GetVowelNumber(String DRIVERNAME)
+        {
+            return DRIVERNAME.Count(driverNameString => "AaEeIiOoUu".Contains(Char.ToLower(driverNameString)));
+        }
+
+        public int GetConsonantNumber(String DRIVERNAME)
+        {
+            return DRIVERNAME.Count(driverNameString => "BbCcDdFfGgHhJjKkLlMmNnPpQqRrSsTtVvWwXxYyZz".Contains(Char.ToLower(driverNameString)));
+        }
+
+        public List<int> GetCommonFactor(int NUMBER)
+        {
+            var factors = new List<int>();
+            int max = (int)Math.Sqrt(NUMBER);
+
+            for (int factor = 1; factor <= max; ++factor)
+            {
+                if (NUMBER % factor == 0)
+                {
+                    factors.Add(factor);
+                    if (factor != NUMBER / factor)
+                        factors.Add(NUMBER / factor);
+                }
+            }
+            return factors;
+        }
+    }
+}
---
+++ AcmeIncAlgorithm/AcmeIncAlgorithm/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+namespace AcmeIncAlgorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insert Address Shipment");
+            String shipmentDestination =  Console.ReadLine();
+            Console.WriteLine("Address Shipment:  " + shipmentDestination);
+
+            Console.WriteLine("Insert Driver Name");
+            String driverName = Console.ReadLine();
+            Console.WriteLine("Driver Name:  " + driverName);
+
+            AcmeIncAlgorithmOperations _operators = new AcmeIncAlgorithmOperations();
+            if(shipmentDestination.Length%2 == 0)
+            {
+                Console.WriteLine("Number Is Even");
+                int vowelCount = _operators.GetVowelNumber(driverName);
+                double baseSS = vowelCount * 1.5;
+
+                var commonNumberMatches = _operators.GetCommonFactor(shipmentDestination.Length).Intersect(_operators.GetCommonFactor(vowelCount));
+                if (commonNumberMatches.Count() > 1)
+                {
+                    Console.WriteLine("Algorithm Bonus Activated!!");
+                    baseSS = baseSS + (baseSS * 0.5);                   
+                }
+                Console.WriteLine("Base SS is =  " + baseSS);
+            }
+            else
+            {
+                Console.WriteLine("Number Is Odd");
+                int consonantCount = _operators.GetConsonantNumber(driverName);
+                double baseSS = consonantCount;
+
+                var commonNumberMatches = _operators.GetCommonFactor(shipmentDestination.Length).Intersect(_operators.GetCommonFactor(consonantCount));
+                if (commonNumberMatches.Count() > 1)
+                {
+                    Console.WriteLine("Algorithm Bonus Activated!!");
+                    baseSS = baseSS + (baseSS * 0.5);
+                }
+                Console.WriteLine("Base SS is =  " + baseSS);
+            }
+            Console.WriteLine("Press Any Key To Continue");
+            Console.ReadKey();
+        }
+    }
+}
